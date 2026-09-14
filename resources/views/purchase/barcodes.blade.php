f412759e94fd9c0a583c69b78a2efa4f31ae7682
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-14 16:35:39 +0500

    Update barcodes.blade.php

--- resources/views/purchase/barcodes.blade.php
+++ resources/views/purchase/barcodes.blade.php
@@ -288,7 +288,7 @@
             width: var(--barcode-w);
             flex-shrink: 0;
             display: block;
-            
+            gap: 0.3mm;
         }
         .unit-barcode svg {
             width: 100%;
