3e6d3df1612f96456a0ac13b5f0842f48d41a3d5
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-21 14:01:59 +0500

    F

--- resources/views/purchase/barcodes.blade.php
+++ resources/views/purchase/barcodes.blade.php
@@ -205,7 +205,7 @@
             display: flex;
             flex-direction: column;
             position: relative;
-            padding: 1.6mm 1.8mm;
+            padding: 1.1mm 1.8mm;
             font-family: var(--mono);
         }
         /* soft guide down the middle, screen only — this is where you tear
@@ -304,7 +304,7 @@
         .unit:nth-child(2) .unit-barcode {
             margin-left: auto;
             margin-right: 0;
-            margin-bottom: 5mm;
+            margin-bottom: 10mm;
         }
         .tag-cert {
             display: flex;
