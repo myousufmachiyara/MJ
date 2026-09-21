03d14704949814e0955b698215d5af0bd1ef91d5
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-21 14:45:49 +0500

    Update barcodes.blade.php

--- resources/views/purchase/barcodes.blade.php
+++ resources/views/purchase/barcodes.blade.php
@@ -304,7 +304,7 @@
         .unit:nth-child(2) .unit-barcode {
             margin-left: auto;
             margin-right: 0;
-            margin-bottom: 15mm;
+            margin-bottom: 20mm;
         }
         .tag-cert {
             display: flex;
