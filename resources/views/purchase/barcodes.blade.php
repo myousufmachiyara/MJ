ccd2fb9c756d4e4527de656fb7cd87f10578efb8
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-23 14:15:29 +0500

    F

--- resources/views/purchase/barcodes.blade.php
+++ resources/views/purchase/barcodes.blade.php
@@ -579,7 +579,7 @@
                     // shorter barcode text (e.g. the newer {code}-00001 codes)
                     // will scan more reliably than the long legacy ones.
                     width:        10,
-                    height:       100,
+                    height:       120,
                     displayValue: false,
                     marginTop:    0,
                     marginBottom: 0,
