1f646775cfd636de6ffc85d8eef1f3ae67674f20
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-23 13:42:25 +0500

    Update barcodes.blade.php

--- resources/views/purchase/barcodes.blade.php
+++ resources/views/purchase/barcodes.blade.php
@@ -561,7 +561,7 @@
                     // gets it as scannable as this physical width allows, but
                     // shorter barcode text (e.g. the newer {code}-00001 codes)
                     // will scan more reliably than the long legacy ones.
-                    width:        5,
+                    width:        10,
                     height:       60,
                     displayValue: false,
                     marginTop:    0,
