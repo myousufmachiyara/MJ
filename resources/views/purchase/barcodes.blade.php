9cc54a1770be6d5f0721f5868bba1feb4e8ee53a
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-23 16:36:20 +0500

    Update barcodes.blade.php

--- resources/views/purchase/barcodes.blade.php
+++ resources/views/purchase/barcodes.blade.php
@@ -34,7 +34,7 @@
             --label-w:   83mm;
             --label-h:   37mm;
             --unit-w:    41.5mm;
-            --barcode-w: 12mm; /* the physical max this half-tag can hold (unit is 41.5mm minus 1.8mm padding each side); pushed to the ceiling to give the barcode every fraction of a mm it can get */
+            --barcode-w: 18mm; /* the physical max this half-tag can hold (unit is 41.5mm minus 1.8mm padding each side); pushed to the ceiling to give the barcode every fraction of a mm it can get */
         }
 
         * { box-sizing: border-box; margin: 0; padding: 0; }
@@ -578,8 +578,8 @@
                     // gets it as scannable as this physical width allows, but
                     // shorter barcode text (e.g. the newer {code}-00001 codes)
                     // will scan more reliably than the long legacy ones.
-                    width:        10,
-                    height:       150,
+                    width:        2,
+                    height:       60,
                     displayValue: false,
                     marginTop:    0,
                     marginBottom: 0,
