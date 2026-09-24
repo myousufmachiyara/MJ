f4261fba89e3aae14daeb362816687a63b2e70e4
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-24 10:46:19 +0500

    Update barcodes.blade.php

--- resources/views/purchase/barcodes.blade.php
+++ resources/views/purchase/barcodes.blade.php
@@ -299,7 +299,7 @@
         }
         .unit-barcode svg {
             width: auto;
-            height: 20px;
+            height: auto;
             display: block;
             /* FIX (unscannable barcode): forces the browser/printer to keep
                each bar's edges pixel-crisp instead of anti-aliasing them as
