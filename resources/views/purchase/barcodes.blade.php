3dcf157d6b2d84d24269fb99103f853e9e1316c5
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-24 11:04:25 +0500

    F

--- resources/views/purchase/barcodes.blade.php
+++ resources/views/purchase/barcodes.blade.php
@@ -593,7 +593,7 @@
                 });
                 el.setAttribute('viewBox', '0 0 70 20');
                 el.setAttribute('height', '20');
-                el.setAttribute('height', '70');
+                el.setAttribute('width', '70');
                 // el.setAttribute('viewBox', `0 0 ${el.getBBox().width} ${el.getBBox().height}`);
             } catch (e) {
                 el.parentElement.innerHTML = '<div style="font-size:8px;color:#c00;text-align:center;">Invalid barcode</div>';
