07645dd1d979f7249a27ff6be2774922088f23c3
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-24 12:17:33 +0500

    Update barcodes.blade.php

--- resources/views/purchase/barcodes.blade.php
+++ resources/views/purchase/barcodes.blade.php
@@ -359,9 +359,7 @@
             .unit-index, .unit-select-wrap { display: none !important; }
             .unit + .unit { border-left: none; } /* screen-only guide, nothing to print here */
         }
-        g{
-            transform: translate(0,0);
-        }
+
     </style>
 </head>
 <body>
